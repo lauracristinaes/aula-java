ce0b2b5201ea06e12f90b6fef9deb2d2456ee22d
laura <lauracristinaes@gmail.com>
2018-09-21 20:12:43 -0300

    commit test

---
+++ Aula-2109/src/obj/Bolo.java
@@ -0,0 +1,14 @@
+package obj;
+
+public class Bolo {
+	
+	public String cobertura, recheio;
+	public double peso, valor;
+	
+	public void distribuiPedacos() {
+		int qntPedacos = 12;
+		
+		System.out.println();
+	}
+
+}
---
+++ Aula-2109/src/obj/ExecutaprogramaBolo.java
@@ -0,0 +1,59 @@
+package obj;
+
+import java.util.Scanner;
+
+public class ExecutaprogramaBolo {
+	
+	public static void main(String[] args) {
+		
+		Bolo meusBolos[] = cadastrar();
+		Imprimir(meusBolos);
+		
+		
+		
+	}
+	
+	public static void Imprimir(Bolo bolos[]) {
+		
+		for(int i = 0; i < 2; i++) {
+
+			System.out.println("recheio do bolo:" + bolos[i].recheio);			
+			System.out.println("peso do bolo:" + bolos[i].peso + "gramas");
+		}
+		
+		
+	}
+	
+	
+	public static Bolo[] cadastrar() {
+		
+		Integer contador = 0;
+		Bolo bolos[] = new Bolo[2];
+		Scanner ler;
+		
+		while (contador != 2) {
+			
+			ler = new Scanner(System.in);
+			//referencia
+			Bolo bolo;
+			
+			//construindo objeto
+			//construtor
+			
+			bolo = new Bolo();
+			
+			System.out.println("digite o sabor:");
+			bolo.recheio = ler.nextLine();
+			System.out.println("digite o peso:");
+			bolo.peso = ler.nextDouble();
+			bolos[contador] = bolo;
+			System.out.println("Bolo adicionado com sucesso");
+			
+			contador++; //contador +=1
+			
+		}
+		
+		return bolos;
+	} 
+
+}
