ca27955cc61b8a4c64924f66327cf70715a7afe9
laura <lauracristinaes@gmail.com>
2018-09-27 19:07:00 -0300

    Exercício Pizzaria: testando construtores

--- Aula-2509/src/pizzaria/Pizza.java
+++ Aula-2509/src/pizzaria/Pizza.java
@@ -17,6 +17,13 @@ public class Pizza {
 		}
 	}
 	
+	public Pizza (int tamanho, Sabor sabor, boolean borda, String saborBorda) {
+		this.qtdPedacos = tamanho;
+		this.sabor = sabor;
+		this.borda = borda;
+		this.saborBorda = saborBorda;
+	}
+	
 	public void selecionarSabor(Sabor sabor) {
 		this.sabor = sabor;
 	}
