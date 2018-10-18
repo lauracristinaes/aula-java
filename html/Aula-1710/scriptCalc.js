5a9defa185233a11a35aa556d38c77249b2f7731
laura <lauracristinaes@gmail.com>
2018-10-17 21:42:56 -0300

    Aula Javascript + Ex calculadora

---
+++ html/Aula-1710/scriptCalc.js
@@ -0,0 +1,30 @@
+
+
+function Calc(){
+
+
+var op = document.getElementById("operator").value;
+	console.log(op);
+
+var n1 = document.getElementById("num1").value;
+
+var n2 = document.getElementById("num2").value;
+
+
+	var resultado;
+
+	if(op == "1"){
+		resultado = parseInt(n1) + parseInt(n2);
+
+	}else if(op == "2"){
+		resultado = n1 - n2;
+
+	}else if(op == "3"){
+		resultado = n1 * n2;
+
+	}else if(op == "4"){
+		resultado = n1 / n2;
+
+	}
+	document.getElementById("resultado").innerHTML = resultado;
+}
---
+++ html/Aula-1710/scriptMain.js
@@ -0,0 +1,87 @@
+function myFunction(){
+	document.getElementById("demo").innerHTML = "Testando JavaScript";
+	alert("Testando JS");
+	console.log("Testando JS");
+}
+
+function Soma(num1, num2){
+	return num1 + num2;
+}
+
+var cor = "vermelho";
+console.log("A cor é " + cor);
+
+var tamanho, forma;
+tamanho = "grande";
+forma = "circular";
+console.log("O tamanho é " + tamanho +" e o formato é " + forma );
+
+
+var IsMember = "false";
+
+IsMember = 1;
+
+IsMember = "Teste";
+
+console.log(IsMember);
+
+var option = "It's alright";
+console.log(option);
+
+option = 'It"s alright';
+console.log(option);
+
+option = 'It\'s alright';
+console.log(option);
+
+var val1 = 22;
+var val2 = 23;
+console.log(val1 + val2);
+
+var val3 = 22.5;
+var val4 = 23.5;
+console.log(val3 + val4);
+
+ val3 = 50;
+ val4 =.6;
+console.log(val3 + val4);
+
+var val3 = 25;
+var val4 = "25";
+console.log(val3 + val4);
+
+var myName;
+console.log(myName);
+
+myName = null;
+console.log(myName);
+
+console.log("doing assigment");
+myName = "Catie";
+console.log(myName);
+
+
+console.log("doing aritmetic");
+console.log(5 + 5);
+console.log(5 - 5);
+console.log(5 * 5);
+console.log(5 / 5);
+console.log(5 % 5);
+console.log(11 % 10);
+
+
+console.log("doing comparisons");
+console.log(11 > 0);
+console.log(11 < 10);
+console.log(10 >= 10);
+console.log(11 <= 10);
+console.log(11 % 10);
+
+console.log("doing string concatenation");
+console.log(myName + " Grant");
+
+console.log("doing boolean logic");
+console.log(true && true);
+console.log(false && false);
+console.log(true || true);
+console.log(true || false);
