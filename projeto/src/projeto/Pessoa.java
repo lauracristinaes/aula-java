0d7e8cfdc5d665b3ef894d7a9e2d5fb9e06817c2
laura <lauracristinaes@gmail.com>
2018-10-29 17:28:06 -0300

    servet

---
+++ projeto/src/projeto/Pessoa.java
@@ -0,0 +1,120 @@
+package projeto;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.naming.NamingException;
+
+
+
+import projeto.BancoDados;
+
+public class Pessoa {
+	
+	//cpf int(11) primary key,
+	//nome varchar(80) not null,
+	//sexo varchar(1) not null,
+	//dtNasc date
+	
+	public int cpf;
+	public String nome;
+	public String sexo; //F ou M
+	//private java.sql.Date dtNasc;
+	public Date dtNasc;
+	
+	public Pessoa(int cpf, String nome, String sexo, Date dt) {
+		
+		this.cpf = cpf;
+		this.nome = nome;
+		this.sexo = sexo;
+		this.dtNasc = dt;
+		
+	}
+	
+	public void addBanco() {
+		
+	
+		
+		BancoDados db = null;
+		try {
+			
+			db = new BancoDados("aulajava");//LOCALHOST
+			
+		} catch (NamingException e) {
+			System.out.println("Erro ao instanciar o Banco de Dados: " + e);
+		}
+
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+	
+			conn = db.obterConexao();
+			
+			
+	
+			String sql = "INSERT INTO pessoa (cpf, nome, sexo, dtNasc) values (?, ?, ?, ?);";
+			
+			
+			
+			
+			
+			
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, this.cpf);
+			stmt.setString(2, this.nome);
+			stmt.setString(3, this.sexo);
+			
+			java.sql.Date d = new java.sql.Date(dtNasc.getTime());
+			stmt.setDate(4, d);
+			//stmt.setDate(4, (java.sql.Date) this.dtNasc);
+				
+			
+			
+			
+		
+			
+			stmt.execute();
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		} finally {
+			db.finalizaObjetos(rs, stmt, conn);
+
+		}
+
+	}
+	
+	public void BuscarPessoaBanco() {
+		
+		
+	}
+	
+	public static void main(String[] args) throws ParseException {
+		
+		int cpf = 000;
+		String nome = "teste";
+		String sexo = "F";
+		SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+		String strDt = "2015-11-15";
+		Date dt = formato.parse(strDt);
+		
+		Pessoa pessoa = new Pessoa(cpf, nome, sexo, dt);
+		pessoa.addBanco();
+	}
+}
---
+++ projeto/src/projeto/TrataForm.java
@@ -0,0 +1,78 @@
+package projeto;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/adicionaContato")
+public class TrataForm extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	
+		PrintWriter	out	=	response.getWriter();
+		
+		String	CPF	=	request.getParameter("CPF");
+		String	nome	=	request.getParameter("nome");
+		String	sexo	=	request.getParameter("sexo");
+		//String	dtNasc	=	request.getParameter("dtNasc");
+		/*
+		
+		String	dataEmTexto	= request.getParameter("dtNasc");
+		Calendar dataNascimento	=	null;
+		//	fazendo	a	conversão	da	data
+		try	{
+			Date date = new	SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento	=	Calendar.getInstance();
+			dataNascimento.setTime(date);
+			}	catch	(ParseException	e)	{
+				out.println("Erro	de	conversão	da	data");
+				return;	//para	a	execução	do	método
+				}
+		*/
+		
+		SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+		String strDt = "2015-11-15";
+		Date dt = null;
+		try {
+			dt = formato.parse(strDt);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int cpf = Integer.parseInt(CPF);
+		
+		Pessoa pessoa = new Pessoa(cpf, nome, sexo, dt);
+		
+		System.out.println(pessoa.cpf);
+		System.out.println(pessoa.nome);
+		System.out.println(pessoa.dtNasc);
+		
+		pessoa.addBanco();
+		
+		
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contato	"	+	nome	+"	adicionado	com	sucesso");								
+		out.println("</body>");
+		out.println("</html>");
+
+
+	}
+
+}
